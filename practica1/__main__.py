a01ab7af25de832c5239bfa52a25269d7f5b10ca
Sergio Vega <VG.Sergio@hotmail.com>
2023-10-17 22:40:21 +0200

    Path

--- practica1/__main__.py
+++ practica1/__main__.py
@@ -1,11 +1,11 @@
 import sys
-sys.path.append("C:\\Users\\Sergio\\Desktop\\ia_2023")
+sys.path.append("C:\\Users\\Sergio\\Desktop\\IA\\ia_2023")
 
 from practica1 import agent, joc
 
 
 def main():
-    quatre = joc.Taulell([agent.AgentProfunditat("O")])
+    quatre = joc.Taulell([agent.AgentProfunditat("CPU_1")])
     quatre.comencar()
 
 
